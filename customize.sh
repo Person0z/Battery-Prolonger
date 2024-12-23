0f82d89880a2aa5752075d90d93dfcc9b9051368
Person0z <80363995+Person0z@users.noreply.github.com>
2024-12-23 14:31:59 -0600

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -1,13 +1,19 @@
 #!/system/bin/sh
 
-# Detect CPU core count
-CPU_CORE_PATH="/sys/devices/system/cpu"
-CPU_CORES=$(ls $CPU_CORE_PATH | grep -E 'cpu[0-9]+' | wc -l)
+# Displaying Module Banner
+ui_print " "
+ui_print "  ____        _   _                    _____           _                              "
+ui_print " |  _ \      | | | |                  |  __ \         | |                             "
+ui_print " | |_) | __ _| |_| |_ ___ _ __ _   _  | |__) | __ ___ | | ___  _ __   __ _  ___ _ __  "
+ui_print " |  _ < / _\` | __| __/ _ \ '__| | | | |  ___/ '__/ _ \| |/ _ \| '_ \ / _\` |/ _ \ '__| "
+ui_print " | |_) | (_| | |_| ||  __/ |  | |_| | | |   | | | (_) | | (_) | | | | (_| |  __/ |    "
+ui_print " |____/ \__,_|\__|\__\___|_|   \__, | |_|   |_|  \___/|_|\___/|_| |_|\__, |\___|_|    "
+ui_print "                                __/ |                                 __/ |          "
+ui_print "                               |___/                                 |___/           "
+ui_print " "
 
-# Log detected core count
-echo "Detected $CPU_CORES CPU cores"
-
+# Setting Permissions
 ui_print "Setting permissions for service.sh..."
 set_perm $MODPATH/service.sh 0 0 0755
 
-ui_print "CPU Powersave Module Installed Successfully."
+ui_print "Battery Prolonger Module Installed Successfully."
